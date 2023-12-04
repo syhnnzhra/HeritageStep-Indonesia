4e9e0518e36a6ada89ff84bcdd3fcf1f7e875891
MuhammadDaryFebrian <muhammaddary88@gmail.com>
2023-12-04 16:09:43 +0700

    menyelesaikan halaman user

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('login', function () {
+    return view('login');
+});
+
 Route::get('home', function () {
     return view('home');
 });

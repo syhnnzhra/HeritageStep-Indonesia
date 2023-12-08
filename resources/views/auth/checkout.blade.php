149ad7a19da50f274986ab91f658a30e03af0fb7
MuhammadDaryFebrian <muhammaddary88@gmail.com>
2023-12-08 10:46:57 +0700

    halaman checkout

---
+++ resources/views/auth/checkout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Keranjang</title>
+    <link rel="stylesheet" href="css/checkout.css"/>
+</head>
+<body>
+    <div class="keranjang-ABw" id="64:337">
+        <p class="keranjang-w6D" id="64:338">Keranjang</p>
+        <div class="card-product-container-2vH" id="64:340">
+        <div class="frame-20-w1f" id="I64:340;64:112">
+        <div class="frame-18-Sz1" id="I64:340;64:112;64:107">
+        <img class="rectangle-39-PeM" src="/api/prod-ap-southeast-1-first-cluster/projects/V7..." id="I64:340;64:112;64:91"/>
+        <div class="frame-16-Wiy" id="I64:340;64:112;64:105">
+        <div class="frame-15-TPK" id="I64:340;64:112;64:104">
+        <div class="frame-14-99f" id="I64:340;64:112;64:103">
+        <p class="category-tN9" id="I64:340;64:112;64:92">Sepatu</p>
+        <p class="product-name-cZ3" id="I64:340;64:112;64:93">Nike Air Jordan</p>
+        </div>
+        <p class="quis-autem-vel-eum-iure-reprehenderit-qui-in-ea-voluptate-velit-esse-quam-nihil-molestiae-consequatu-wLR" id="I64:340;64:112;64:94">Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatu</p>
+        </div>
+        <div class="frame-13-DYq" id="I64:340;64:112;64:102">Rp.2.900.000</div>
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="frame-8-pHj" id="64:401">
+        <img class="bag-dash-fill-1-ie1" src="/api/prod-ap-southeast-1-first-cluster/projects/V7..." id="64:374"/>
+        <div class="group-7-s17" id="64:372">
+        <div class="auto-group-hv3f-czH" id="VWaY4bvkipyXgc3iinhv3f">
+        <div class="line-6-xHT" id="64:366"></div>
+        <p class="item-1-4LV" id="64:371">1</p>
+        <img class="frame-kz1" src="/api/prod-ap-southeast-1-first-cluster/projects/V7..." id="64:359"/>
+        </div>
+        <img class="dash-circle-fill-1-Vgh" src="/api/prod-ap-southeast-1-first-cluster/projects/V7..." id="64:363"/>
+        </div>
+        <img class="bag-check-fill-1-EPP" src="/api/prod-ap-southeast-1-first-cluster/projects/V7..." id="64:399"/>
+        </div>
+        <p class="total-harga-meD" id="64:402">Total Harga</p>
+        <div class="auto-group-zukq-7CH" id="VWaXRTKzAAb54UTCz7Zukq">
+        <p class="rp2900000-q8H" id="64:403">Rp.2.900.000</p>
+        <div class="primary-button-container-N8D" id="64:404">Beli</div>
+        </div>
+        </div>
+    
+</body>
+</html>

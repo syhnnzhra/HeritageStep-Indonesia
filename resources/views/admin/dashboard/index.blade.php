23bb35a2c5be6de6afa7be1c7043da0f873f81a1
Fathia Maulida <fathiamaulida@Fathias-MacBook-Air.local>
2023-12-17 14:11:34 +0700

    tambah table di dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -26,11 +26,12 @@
 
     <div>
     <div class="uk-overflow-auto">
-    <table class="uk-table uk-table-hover uk-table-middle uk-table-divider underline-offset-auto my-5 mx-5">
+    <table class="uk-table uk-table-hover uk-table-middle uk-table-divider my-5 mx-5">
+        
         <thead>
             <tr>
                 <th class="uk-table-shrink"></th>
-                <caption>Laporan Pengiriman</caption>
+                <caption>pesanan baru-baru ini</caption>
                 <th class="uk-table-expand">Barang</th>
                 <th class="uk-width-small">Nama Barang</th>
                 <th class="uk-table-shrink uk-text-nowrap">Harga Barang</th>
@@ -51,6 +52,15 @@
                 <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
                 <td><div class="image-wrapper"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
                 <td class="uk-table-link">
+                    <a class="uk-link-reset" href="">Baju</a>
+                </td>
+                <td class="uk-text-truncate">200.000</td>
+                <td class="uk-text-nowrap">Diproses</td>
+            </tr>
+            <tr>
+                <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
+                <td><div class="uk-preserve-width uk-border-circle"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
+                <td class="uk-table-link">
                     <a class="uk-link-reset" href="">Sepatu</a>
                 </td>
                 <td class="uk-text-truncate">200.000</td>
@@ -67,7 +77,59 @@
             </tr>
         </tbody>
     </table>
-
-
+<!-- -------------------------------------------------------------------------------->
+    <div class="uk-overflow-auto">
+    <table class="uk-table uk-table-hover uk-table-middle uk-table-divider my-5 mx-5">
+    <caption>Laporan Pengiriman</caption>
+        <thead>
+            <tr>
+                <th class="uk-table-shrink"></th>
+                <!-- <caption>Laporan Pengiriman</caption> -->
+                <th class="uk-table-shrink">Barang</th>
+                <th class="uk-table-expand">Nama Barang</th>
+                <th class="uk-width-small">Harga Barang</th>
+                <th class="uk-table-shrink uk-text-nowrap">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+             <tr>
+                <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
+                <td><div class="image-wrapper"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
+                <td class="uk-table-link">
+                    <a class="uk-link-reset" href="">Baju</a>
+                </td>
+                <td class="uk-text-truncate">200.000</td>
+                <td class="uk-text-nowrap">Diproses</td>
+            </tr>
+            <tr>
+                <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
+                <td><div class="image-wrapper"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
+                <td class="uk-table-link">
+                    <a class="uk-link-reset" href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                </td>
+                <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+                <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
+                <td><div class="image-wrapper"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
+                <td class="uk-table-link">
+                    <a class="uk-link-reset" href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                </td>
+                <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+            </tr>
+            <tr>
+                <!-- <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td> -->
+                <td><div class="image-wrapper"><img class="image" src="css/assets/rectangle-39.png" width="80" height="80"/></div></td>
+                <td class="uk-table-link">
+                    <a class="uk-link-reset" href=""></a>
+                </td>
+                <td class="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                <td class="uk-text-nowrap">Lorem ipsum dolor</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 @endsection

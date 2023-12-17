92b98a05d053b0b007dbd327f5c27013c5f96d63
Syahnan <syhnnzhra@gmail.com>
2023-12-17 13:56:36 +0700

    ganti design sign up

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,78 +1,119 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta charset="utf-8" />
-  <link rel="icon" href="/favicon.ico" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#000000" />
-  <title>Sign in</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A300%2C400%2C500"/>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C500"/>
-  <link rel="stylesheet" href="css/signup.css"/>
-  <!-- UIkit CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
+	<title>HeritageStep | Sign Up</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="icon" type="image/png" href="auth/images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="auth/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="auth/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="auth/vendor/animate/animate.css">	
+	<link rel="stylesheet" type="text/css" href="auth/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="auth/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="auth/vendor/select2/select2.min.css">	
+	<link rel="stylesheet" type="text/css" href="auth/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="auth/css/util.css">
+	<link rel="stylesheet" type="text/css" href="auth/css/main.css">
 
-  <!-- UIkit JS -->
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js"></script>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js"></script>
 </head>
 <body>
-<div class="sign-in-fuf">
-  <div class="auto-group-tvc5-zS9">
-    <img class="image-1-WQV" src="css/assets/image-1-t7f.png"/>
-    <p class="daftar-sekarang-2td">Daftar Sekarang</p>
-  </div>
-  <div class="auto-group-csqk-wVo">
-    <div class="auto-group-wyqf-Gny">
-      <img class="image-15-ppV" src="css/assets/image-15.png"/>
-      <img class="image-16-yBb" src="css/assets/image-16.png"/>
-    </div>
-    <div class="auto-group-elfo-u5F">
-      <div class="primary-button-container-d1F">
-        <a href="/auth/google">Google</a>
-      </div>
-      <div class="primary-button-container-d1F">
-        <a href="#">Facebook</a>
-      </div>
-    </div>
-  </div>
-  <div class="group-6-ZJD">
-    <div class="line-4-fs3">
-    </div>
-    <p class="atau-daftar-dengan-1AD">Atau Daftar dengan</p>
-    <div class="line-5-9GR">
-    </div>
-  </div>
-    <form action="/register" method="post">
-        @csrf
-        <input type="hidden" name="provider" value="local">
-        <input type="hidden" name="role" value="user">
-        <div class="group-5-HNd">
-            <p class="phone-number-or-email-cfo">Name</p>
-            <input type="text" name="name" class="rectangle-47-M7b @error('name') is-invalid @enderror" required>
-            @error('name')
-                <div class="uk-alert-success" uk-alert>
-                    <a href class="uk-alert-close" uk-close></a>
-                    <p>{{ $message }}</p>
-                </div>
-            @enderror
-        </div>
-        <div class="group-5-HNd">
-            <p class="phone-number-or-email-cfo">Email</p>
-            <input type="email" name="email" class="rectangle-47-M7b @error('email') is-invalid @enderror" required>
-            @error('email')
-                <div class="uk-alert-success" uk-alert>
-                    <a href class="uk-alert-close" uk-close></a>
-                    <p>{{ $message }}</p>
-                </div>
-            @enderror
-        </div>
-        <div class="group-5-HNd">
-            <p class="phone-number-or-email-cfo">Password</p>
-            <input type="password" name="password" class="rectangle-47-M7b" required>
-            <p class="example-heritagegmailcom-Ui1">Sudah punya akun? <a href="/login">Sign In</a> Sekarang!</p>
-        </div>
-        <button type="submit" class="primary-button-container-Qbf"> Sign Up</button>
-    </form>
-</div>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" action="/register" method="post">
+                @csrf
+                    @if(session()->has('success'))
+                        <div class="uk-alert-success" uk-alert>
+                            <a href class="uk-alert-close" uk-close></a>
+                            <p>{{ session('success') }}</p>
+                        </div>
+                    @endif
+                    @if(session()->has('loginError'))
+                        <div class="uk-alert-danger" uk-alert>
+                            <a href class="uk-alert-close" uk-close></a>
+                            <p>{{ session('loginError') }}</p>
+                        </div>
+                    @endif
+                    @if(session()->has('error'))
+                        <div class="uk-alert-danger" uk-alert>
+                            <a href class="uk-alert-close" uk-close></a>
+                            <p>{{ session('error') }}</p>
+                        </div>
+                    @endif
+					<span class="login100-form-title p-b-49">
+						Sign In
+					</span>
+
+					<input type="hidden" name="provider" value="local">
+        			<input type="hidden" name="role" value="user">
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Name is reauired">
+						<span class="label-input100">Nama</span>
+						<input class="input100 @error('name') is-invalid @enderror" type="text" name="name" placeholder="Type your name" required>
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+                        @error('name')
+                            <span class="uk-form-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Email is reauired">
+						<span class="label-input100">Email</span>
+						<input class="input100 @error('email') is-invalid @enderror" type="text" name="email" placeholder="Type your Email" required>
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+                        @error('email')
+                            <span class="uk-form-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="password" placeholder="Type your password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<div class="text-right p-t-8 p-b-31">
+                        Already have an account? 
+						<a href="#">
+							Sign In
+						</a>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" type="submit">
+								Sign Up
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+	<script src="auth/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="auth/vendor/animsition/js/animsition.min.js"></script>
+	<script src="auth/vendor/bootstrap/js/popper.js"></script>
+	<script src="auth/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="auth/vendor/select2/select2.min.js"></script>
+	<script src="auth/vendor/daterangepicker/moment.min.js"></script>
+	<script src="auth/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="auth/vendor/countdowntime/countdowntime.js"></script>
+	<script src="auth/js/main.js"></script>
+
 </body>
+</html>

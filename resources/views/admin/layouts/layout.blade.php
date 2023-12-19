cd7882f8d4b68a2692945ca5a332194fea4339de
Fathia Maulida <fathiamaulida@Fathias-MacBook-Air.local>
2023-12-19 20:37:52 +0700

    benerin dashboard

---
+++ resources/views/admin/layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<style>
+
+    
+</style>
+<body>
+
+</body>
+</html>
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -10,6 +10,7 @@
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js"></script>
 <title>HERITAGE_STEP Dashboard</title>
 </head>
+
 <body class="flex h-screen bg-gray-100">
   <div class="bg-green-800 text-white w-64 p-4 custom-warna ">
     <h2 class="text-2xl font-bold mb-4">HERITAGE_STEP</h2>

b33a4750ad68da1a6217d7e7dd7a87291c6566c3
Fathia Maulida <fathiamaulida@Fathias-MacBook-Air.local>
2023-12-22 18:03:51 +0700

    benerin create

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,7 +11,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return view('admin.customer.index');
+        return view('admin.customers.index');
     }
 
     /**
